b6b66075d2517c3541d544a44b50c8fa5ef498cb
ikepu-tp <postmaster@licsss.jp>
2023-08-27 22:10:06 +0900

    Add: FactoryTest(実行できない）

---
+++ tests/Unit/ModelTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Account;
+use App\Models\Account_item;
+use App\Models\Account_payment;
+use App\Models\Association;
+use App\Models\Category;
+use App\Models\Check;
+use App\Models\Item;
+use App\Models\Member;
+use App\Models\Payment;
+use App\Models\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ModelTest extends TestCase
+{
+    /*
+    public function test_Association_factory_is_ok()
+    {
+        $this->assertTrue(Association::factory()->create() ? true : false);
+    }
+    public function test_Project_factory_is_ok()
+    {
+        $this->assertTrue(Project::factory()->create() ? true : false);
+    }
+    public function test_Member_factory_is_ok()
+    {
+        $this->assertTrue(Member::factory()->create() ? true : false);
+    }
+    public function test_Payment_factory_is_ok()
+    {
+        $this->assertTrue(Payment::factory()->create() ? true : false);
+    }
+    public function test_Category_factory_is_ok()
+    {
+        $this->assertTrue(Category::factory()->create() ? true : false);
+    }
+    public function test_Item_factory_is_ok()
+    {
+        $this->assertTrue(Item::factory()->create() ? true : false);
+    }
+    public function test_Account_factory_is_ok()
+    {
+        $this->assertTrue(Account::factory()->create() ? true : false);
+    }
+    public function test_Account_payment_factory_is_ok()
+    {
+        $this->assertTrue(Account_payment::factory()->create() ? true : false);
+    }
+    public function test_Account_item_factory_is_ok()
+    {
+        $this->assertTrue(Account_item::factory()->create() ? true : false);
+    }
+    public function test_check_factory_is_ok()
+    {
+        $this->assertTrue(Check::factory()->create() ? true : false);
+    }
+    */
+}
