24de23be1919135b07d73c701abf311f84f28986
ikepu-tp <postmaster@licsss.jp>
2023-09-09 07:31:06 +0900

    Add: jest

--- resources/react/App.tsx
+++ resources/react/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App(): React.ReactNode {
+function App(): React.ReactElement {
 	return (
 		<div className="App">
 			<header className="App-header">
