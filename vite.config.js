bc66e9bab5f1eea190227e1e4a7f26bcb5cbc1fc
ikepu-tp <postmaster@licsss.jp>
2023-10-01 14:25:03 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/react/index.tsx'],
-            refresh: [...refreshPaths, 'app/Http/Livewire/**', 'resources/react/components/**'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/react/index.tsx',
+                'resources/react/progress.tsx',
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+                'resources/react/components/**',
+            ],
         }),
         react(),
     ],
